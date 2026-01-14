e7c03dec80d655e6f8a0f20fc0181110cedf9481
Annie Kuxa <kuxa.business@gmail.com>
2026-01-14 14:45:20 -0400

    drawn bunny's body

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
-//Hello, Halifax! program
+//Drawing a bunny program
 //Annie Kuxa
-//January 3, 2026
+//January 14, 2026
 
 #include "SSDL.h"
 
 int main (int argc, char** argv)
 {
-    sout << "Hello, Halifax! (Press any key to quit.)\n";
+    SSDL_SetWindowTitle ("Just a little bunny");
+
+    //Setting up colors for the picture
+    SSDL_Color LILAC_SKY = SSDL_CreateColor (200, 190, 230);
+    SSDL_Color CREAM_FUR = SSDL_CreateColor (255, 253, 240); 
+
+    //Paint the backrgound
+    SSDL_RenderClear (LILAC_SKY);
+
+    //Bunny's body
+    SSDL_SetRenderDrawColor     (CREAM_FUR);
+    SSDL_RenderFillCircle       (300, 320, 80);
+
+    //Bunny's head
+    SSDL_RenderFillCircle       (300, 220, 60);
+
+    //Bunny's tail
+    SSDL_RenderFillCircle       (370, 360, 15);
+
     SSDL_WaitKey (); //Waiting for user to hit any key
     
     return 0;
