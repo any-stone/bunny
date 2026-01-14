b6119ff3c88c90f897c958e9b4134aef16e91d1e
Annie Kuxa <kuxa.business@gmail.com>
2026-01-14 15:40:53 -0400

    added face and finalized the picture

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main (int argc, char** argv)
 
     //Setting up colors for the picture
     SSDL_Color LILAC_SKY = SSDL_CreateColor (200, 190, 230);
-    SSDL_Color CREAM_FUR = SSDL_CreateColor (255, 253, 240); 
+    SSDL_Color CREAM_FUR = SSDL_CreateColor (255, 253, 240);
+    SSDL_Color INDIGO    = SSDL_CreateColor (75, 0, 130); 
 
     //Paint the backrgound
     SSDL_RenderClear (LILAC_SKY);
@@ -32,6 +33,25 @@ int main (int argc, char** argv)
     //Top parts of bunny's ears
     SSDL_RenderFillCircle       (272, 120, 12);
     SSDL_RenderFillCircle       (332, 120, 12);
+
+    //Bunny's face
+    SSDL_SetRenderDrawColor     (INDIGO);
+
+    //Eyes
+    //Left
+    SSDL_RenderDrawLine         (260, 200, 270, 190);
+    SSDL_RenderDrawLine         (270, 190, 280, 200);
+
+    //Right
+    SSDL_RenderDrawLine         (310, 200, 320, 190);
+    SSDL_RenderDrawLine         (320, 190, 330, 200);
+
+    //Mouth
+    SSDL_RenderDrawCircle       (290, 230, 10);
+    
+    //Change color to bunny's fur color to create overlapping rectangle
+    SSDL_SetRenderDrawColor     (CREAM_FUR);
+    SSDL_RenderFillRect         (280, 220, 25, 10);
 
     SSDL_WaitKey (); //Waiting for user to hit any key
     
